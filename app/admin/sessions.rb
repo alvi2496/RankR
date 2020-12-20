0f9d5bc9f6a5987b722d5aa64b4209c94736b509
Alvi <alvi.utsab@gmail.com>
2020-12-19 22:43:01 -0800

    add session create

--- app/admin/sessions.rb
+++ app/admin/sessions.rb
@@ -1,18 +1,28 @@
 ActiveAdmin.register Session do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :admin_user_id, :title, :default_session
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:admin_user_id, :title, :default_session]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :admin_user_id, :title, :default_session, 
+                courses_attributes: [:title]
+
+  index do
+    selectable_column
+    column :title
+    actions
+  end
+
+  filter :title
+
+  form do |f|
+    f.inputs do
+      f.input :admin_user_id, input_html: { value: current_admin_user.id }, as: :hidden
+      f.input :title
+      f.input :default_session
+    end
+    f.has_many :courses do |course|
+      course.inputs do
+        course.input :title
+      end
+    end
+    f.actions 
+  end
   
 end
---
+++ app/models/assignments_teams.rb
@@ -0,0 +1,4 @@
+class AssignmentsTeams < ApplicationRecord
+    belongs_to :team
+    belongs_to :assignment
+end
