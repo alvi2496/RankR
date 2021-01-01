d1c7b80ce646099ad08e19f87372729d5749fb5c
alvi2496 <alvi.utsab@gmail.com>
2021-01-01 05:45:42 -0800

    student team: bug fix

--- app/models/students_team.rb
+++ app/models/students_team.rb
@@ -6,6 +6,10 @@ class StudentsTeam < ApplicationRecord
     def self.assign_students(course_id, team_id, student_ids)
         previous_student_teams = StudentsTeam.where(course_id: course_id, team_id: team_id)
         student_ids.each do |student_id|
+            already_assigned = StudentsTeam.where(course_id: course_id, student_id: student_id)
+            unless already_assigned.empty?
+                already_assigned.destroy_all
+            end
             student_team = StudentsTeam.find_or_create_by(course_id: course_id, team_id: team_id, student_id: student_id)
             previous_student_teams = previous_student_teams - [student_team]
         end
