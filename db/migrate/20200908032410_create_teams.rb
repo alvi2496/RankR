a6272fce808422e24f77a04feaa0a4490e870787
alvi2496 <alvi.utsab@gmail.com>
2020-12-31 18:25:39 -0800

    remove unnecessary migrations

--- db/migrate/20200908032410_create_teams.rb
+++ db/migrate/20200908032410_create_teams.rb
@@ -1,10 +1,8 @@
 class CreateTeams < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
-
       t.belongs_to :course
       t.string :name
-
       t.timestamps
     end
   end
--- db/migrate/20201012210412_add_team_grade_to_teams.rb
+++
@@ -1,6 +0,0 @@
-class AddTeamGradeToTeams < ActiveRecord::Migration[6.0]
-  def change
-    add_column :teams, :team_grade, :float, default: 0.0
-    add_column :assignments_students, :individual_grade, :float, default: 0.0
-  end
-end
